6028501ae550a6d0e789790fdb6392afafb8e00f
Abdallah <abdallahalazzam3@gmail.com>
2022-05-05 17:02:05 -0400

    finish edit user information and uplead profile image then show it in navbar

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const multer = require("multer");
 
 const app = express();
 app.use(express.static("public"));
@@ -13,20 +14,41 @@ app.use(session({
 }))
 app.set("view engine", "ejs");
 
+///////////////////////multer///////////////////////
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(
+            null, __dirname + "/public/profileImages"
+        )
+    },
+    filename: (req, file, cb) => {
+        const uniqesuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqesuffix + '-' + file.originalname)
+    }
+})
+
+let upload = multer({ storage: storage })
+
+
+////////////////////////mongooseDatabase///////////////
 mongoose.connect("mongodb://localhost:27017/Connect", { useUnifiedTopology: true, useNewUrlParser: true });
 const userSchema = mongoose.Schema({
     firstName: String,
     lastName: String,
     userName: String,
-    password: String
+    password: String,
+    profileImage: String
 })
 
 const userModel = mongoose.model("user", userSchema)
-
+//////////////////////////////////////////////////////////
 app.listen(3000, () => {
     console.log("the servier is running using port 3000");
 });
 
+
+
+////////////////////////get///////////////////////////////
 app.get("/", (req, res) => {
     res.render(__dirname + "/mainPages/index.ejs")
 });
@@ -47,7 +69,7 @@ app.get("/editInfo", (req, res) => {
 })
 
 
-
+////////////////////post///////////////////////////////////
 app.post("/signin", (req, res) => {
     const { firstName, lastName, userName, password } = req.body;
 
@@ -102,6 +124,14 @@ app.post("/editInfo", async (req, res) => {
     let doc = await userModel.findOneAndUpdate({ id: req.session.user._id },
         { firstName: editInfoFirstName, lastName: editInfoLastName },
         { new: true })
+    req.session.user = doc
+    res.redirect("editInfo");
+})
+
+app.post("/uploadImg", upload.single("profileImage"), async (req, res) => {
+    console.log(req.file.filename);
+    let doc = await userModel.findOneAndUpdate({ id: req.session.user._id },
+        { profileImage: req.file.filename }, { new: true });
     req.session.user = doc
     res.redirect("editInfo");
 })
